5e640923fdd3b4069e5cd273709a8472338c1159
Marc Domenech <markdomkan@hotmail.com>
2022-12-02 11:01:33 +0100

    first commit, ready to refactor ;)

---
+++ CreateUser.ts
@@ -0,0 +1,19 @@
+import { Client } from "https://deno.land/x/mysql@v2.11.0/mod.ts";
+import { query, table } from "https://deno.land/x/sql@1.0/mod.ts";
+import { User } from "./User.ts";
+
+export async function CreateUser(name: string) {
+  const user = new User();
+  user.id = 1;
+  user.name = name;
+  const insert = table("user")
+    .insert({
+      a: 123,
+      b: 456,
+    })
+    .build();
+  const client = await new Client().connect({
+    hostname: "localhost",
+  });
+  await query(client, insert);
+}
